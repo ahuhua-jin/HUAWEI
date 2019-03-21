22188ef42ecffb82101e6f3b157a292616c2e6c6
unknown <1300934639@qq.com>
2019-03-21 20:04:45 +0800

    HUAWEI

--- dest/js/index.js
+++ dest/js/index.js
@@ -56,8 +56,45 @@ $Lol.mouseenter(function(){
 		.siblings()
 		.css('display','none')
 })
-$Lol.mouseleave(function(){
+$Lul.mouseleave(function(){
 	var index = $(this).index();
 	$Lul.eq(index)
 		.css('display','none')
 })
+
+//点击右侧按钮 ：  操作ul的marginLeft值为 -一张图片的宽度
+	$("#Qright").click(function(){
+		$(".Qlist").animate( { marginLeft : -1284} , 1000 ,function(){
+			//运动完成后  marginLeft值恢复到0
+			//将ul中的第一个li（第一张图片）剪切到ul的后面
+			
+		} )
+	})
+	$("#Qleft").click(function(){
+		$(".Qlist").animate( { marginLeft : 0} , 1000 ,function(){
+			//运动完成后  marginLeft值恢复到0
+			//将ul中的第一个li（第一张图片）剪切到ul的后面
+		} )
+	})
+$(function(){
+		var timer = setInterval(auto,2500);
+		var index = 0;
+		function auto(){
+			index++;
+			if( index == $(".Rol li").size() ){
+				index = 0;
+			}
+			$(".Rol li").eq(index).addClass("current").siblings().removeClass("current");
+			$(".Rul li").eq(index).fadeIn(2000).siblings().fadeOut(2000);
+		}
+		
+		//鼠标 操作
+		$(".Rol li").mouseenter(function(){
+			clearInterval(timer);
+			index = $(this).index()-1;
+			auto();
+		})
+		$(".Rol li").mouseout(function(){
+			timer = setInterval(auto,2500);
+		})
+	})
